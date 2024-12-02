23d1bb9d8803639b6788b03a53f29eaca79b1276
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-02 10:50:20 +0530

    Time: 71 ms (37.95%) | Memory: 57 MB (53.57%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.js
+++ 917-boats-to-save-people/boats-to-save-people.js
@@ -6,11 +6,9 @@
 var numRescueBoats = function(people, limit) {
     let count=0
     people.sort((a,b)=>a-b)
-    let t=people.length-1
-    for(let i=t,j=0;i>=j;i--){
+    for(let i=people.length-1,j=0;i>=j;i--){
         if(people[i]+people[j]<=limit){
             count++
-            t--
             j++
         } else{
             count++
