fb1d283391113b8a2a054084cb799ce7e3a3aa34
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-04 09:34:31 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (26.36%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -6,17 +6,14 @@ var dominantIndex = function(nums) {
 let max=-Infinity
 let mind=0
 let sec=-Infinity
-let sind=0
 for(let i=0;i<nums.length;i++){
     if(nums[i]>max){
         sec=max
         max=nums[i]
-        sind=mind
         mind=i
     }
     else if(sec<nums[i] && max>nums[i]){
         sec=nums[i]
-        sind=i
     }
 }
 if(max>=2*sec)return mind
