f5408734ad4cd4b31b774ef4926fbd80c60c55f4
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-05 09:36:06 +0530

    Time: 81 ms (6.28%) | Memory: 58 MB (5.37%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+let lcount = 0
+let acount = 0
+let max=0
+for(let i=0;i<s.length;i++){
+    
+    if(s[i]=="A"){
+        acount+=1
+        lcount=0
+    }
+    else if(s[i]=="L"){
+        lcount+=1
+    } else {
+        lcount=0
+    }
+    console.log(lcount)
+    if(max<lcount){
+        max=lcount
+    }
+}
+if(max>=3 || acount>=2)return false
+else return true
+};
