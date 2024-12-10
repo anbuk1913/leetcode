23d3fc3cd51ebd4c082b5fa97d66e7b03c40d151
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-10 09:36:31 +0530

    Time: 1642 ms (8.07%) | Memory: 51.8 MB (92.30%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let count=0
+    for(let i=0;i<nums.length;i++){
+        let t1=nums[i];
+        if(t1==k){
+            count++
+        }
+        for(let j=i+1;j<nums.length;j++){
+            
+            t1+=nums[j]
+            if(t1==k){
+                count++
+            }
+        }
+    }
+    return count
+};
