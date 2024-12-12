737003001d1674e70b1b28de6b66dd9f463943d9
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-12 09:54:46 +0530

    Time: 52 ms (93.01%) | Memory: 52.6 MB (16.22%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let arr=[]
+    for(let i=0;i<nums.length/2;i++){
+        arr.push(nums[i])
+        arr.push(nums[n+i])
+    }
+    return arr
+};
