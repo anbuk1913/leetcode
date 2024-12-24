4880ccdfea36396625c5e4cd1fc0970924ce682f
Anbu <136681576+anbuk143@users.noreply.github.com>
2024-12-24 09:23:00 +0530

    Time: 5 ms (96.85%) | Memory: 58.8 MB (73.26%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    let i=1
+    let t=1
+    if(n==1)return true
+    while(t<=n){
+        t=1
+        for(let j=1;j<=i;j++)t*=3
+        if(t==n)return true
+        i++
+    }
+    return false
+};
