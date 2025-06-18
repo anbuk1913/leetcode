9d398e2c7251d44c2339968da1525b08f8aa69c4
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-06-18 12:42:32 +0530

    Time: 2 ms (73.70%) | Memory: 58 MB (23.26%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.js
+++ 3778-transform-array-by-parity/transform-array-by-parity.js
@@ -4,11 +4,17 @@
  */
 var transformArray = function(nums) {
     let  ans = []
+    let pos = 0
     for(let i of nums){
-        if(i%2){
-            ans.push(1)
+        if(i%2==0){
+            pos++
+        }
+    }
+    for(let i=0;i<nums.length;i++){
+        if(i<pos){
+            ans.push(0)
         } else {
-            ans.unshift(0)
+            ans.push(1)
         }
     }
     return ans
