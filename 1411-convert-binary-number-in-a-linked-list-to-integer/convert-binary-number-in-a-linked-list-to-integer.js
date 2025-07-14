1cfb68d50f2421f7aa974b9abde1b15c4f7332b6
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-07-14 21:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (66.46%) - LeetSync

---
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let bi = ""
+    while(head){
+        bi+=head.val
+        head = head.next
+    }
+    bi = "0b"+bi
+    return Number(bi)
+};
