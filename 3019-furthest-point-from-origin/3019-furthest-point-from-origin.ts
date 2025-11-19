f1f1d26cc2378c58661b461013d9be21b9a76eef
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-11-19 12:14:29 +0530

    Time: 2 ms (37.5%), Space: 58.3 MB (56.25%) - LeetHub

---
+++ 3019-furthest-point-from-origin/3019-furthest-point-from-origin.ts
@@ -0,0 +1,20 @@
+function furthestDistanceFromOrigin(moves: string): number {
+    let ans = 0
+    let L = 0
+    let R = 0
+    let _ = 0
+    for(let i=0;i<moves.length;i++){
+        if(moves[i]=="L"){
+            L++
+        } else if (moves[i]=="R"){
+            R++
+        } else {
+            _++
+        }
+    }
+    if(L>R){
+        return L-R+_
+    } else {
+        return R-L+_
+    }
+};
