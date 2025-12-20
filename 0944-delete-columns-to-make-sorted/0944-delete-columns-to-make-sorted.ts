e37a64b2f2ad297a22ba9de81c584cf2fcec873b
Anbu <136681576+anbuk1913@users.noreply.github.com>
2025-12-20 16:35:44 +0530

    Time: 6 ms (72.22%), Space: 59.9 MB (22.22%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
@@ -0,0 +1,12 @@
+function minDeletionSize(strs: string[]): number {
+    let ans = 0 
+    for(let i=0;i<strs[0].length;i++){ 
+        for(let j=1;j<strs.length;j++){ 
+            if(strs[j][i]<strs[j-1][i]){ 
+                ans++ 
+                break 
+            } 
+        } 
+    } 
+    return ans
+};
